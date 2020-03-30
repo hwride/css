819c9235f77b10b16bba8319b49a7fb369caa72d
Hugo Wride <hugowride@gmail.com>
2020-03-30 21:13:23 +0100

    Add inline height examples.

--- width-height-and-margins/inline.js
+++ width-height-and-margins/inline.js
@@ -1,7 +1,12 @@
+/* Width and vertical margins */
 createCSSTestingComponent({
 	parent: document.querySelector('.example-inline-width'),
 	html: `<div>
-	<span>Text</span>
+  Text text text text text text 
+  text text text text text
+  <span>text</span>
+  text text text text text text
+  text text text text
 </div>`,
 	css: `
 span {
@@ -12,17 +17,62 @@ span {
 createCSSTestingComponent({
 	parent: document.querySelector('.example-inline-margin'),
 	html: `<div>
-	<span class="auto-margin">Text</span>
-	<span class="length-margin">word</span>
-	text
+  Text text text text text text 
+  text text text text text
+  <span class="text-1">text</span>
+  text text text 
+  <span class="text-2">text</span> 
+  text text text text text text
 </div>`,
 	css: `
-.auto-margin {
+.text-1 {
   background: dodgerblue;
+  border-left: 5px solid red;
+  border-right: 5px solid red;
+  padding: 0 20px 0 20px;
+  margin: 0 50px 0 50px;
+}
+.text-2 {
+  background: red;
   margin: 0 auto 0 auto;
 }
-.length-margin {
+`})
+
+/* Height and horizontal margins */
+createCSSTestingComponent({
+	parent: document.querySelector('.example-inline-height'),
+	html: `<div>
+  Text text text text text text 
+  text text text text text
+  <span class="height">text</span>
+  text text text 
+  <span class="font-2">text</span> 
+  text text text text text text
+</div>`,
+	css: `
+.height {
+  background: dodgerblue;
+  height: 100px;
+}
+.font-2 {
   background: red;
-  margin: 0 50px 0 50px;
+  font-size: 2em;
+}
+`})
+
+createCSSTestingComponent({
+	parent: document.querySelector('.example-inline-vertical-props'),
+	html: `<div>
+  Text text text text text text text text text text text text text text text 
+	<span class="vertical-props">Text</span>
+	text text text text text text text text text text text text text text text
+</div>`,
+	css: `
+.vertical-props {
+  background: dodgerblue;
+  margin: 250px 0 250px 0;
+  padding: 40px 0 40px 0;
+  border-top: 5px solid red;
+  border-bottom: 5px solid red;
 }
 `})
