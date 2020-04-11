1c5ae0b5922630f64a2ac469dad934c8f85c9012
Hugo Wride <hugowride@gmail.com>
2020-04-11 18:17:59 +0100

    Add navigation menu with home link to pages.

---
+++ nav.js
@@ -0,0 +1,15 @@
+/* Common navigation component for the doc pages */
+function createNav(homeHref = '../index.html') {
+	const nav = document.createElement('nav')
+	nav.class = 'css-testing-nav'
+	nav.innerHTML = `
+<ul style="
+    list-style: none;
+    text-align: center;
+    margin-bottom: 0;
+">
+	<li><a href="${homeHref}">Home</a></li>
+</ul>
+`
+	document.querySelector('body').prepend(nav)
+}
