b173a240fddb0c7861b69c8ecf46cc5faa83221b
Hugo Wride <hugowride@gmail.com>
2021-01-01 13:41:13 +0000

    Add some more overflow examples.

--- overflow/index.js
+++ overflow/index.js
@@ -26,8 +26,27 @@ createCSSTestingComponent({
     label: 'overflow: visible (default)',
     reset: true
   }, {
+    label: 'overflow: hidden',
+    description: `<code>overflow: hidden</code> causes the overflowing part of children to be hidden.`,
+    html: `<div class="a blue">
+    <div class="b red"></div>
+</div>`,
+    css: `
+.a {
+  height: 100px;
+  padding: 20px;
+  overflow: hidden;
+}
+.b {
+  height: 200px;
+  width: 100px;
+}`,
+  }, {
     label: 'overflow: auto',
-    description: `<code>overflow: auto</code> causes a scroll bar to appear to scroll to overflowing children.`,
+    description: `<code>overflow: auto</code> causes a scroll bar to appear to scroll to overflowing children. If there 
+is no overflow then no scroll bar will appear.<br/><br/>
+
+Notice the scroll bars are taken out of the space of the block that is scrolling.`,
     html: `<div class="a blue">
     <div class="b red"></div>
 </div>`,
@@ -40,6 +59,110 @@ createCSSTestingComponent({
 .b {
   height: 200px;
   width: 100px;
+}`,
+  }, {
+    label: 'overflow: scroll',
+    description: `Causes a scroll bar to always appear even when children aren't overflowing.`,
+    html: `<div class="a blue">
+    <div class="b red"></div>
+</div>`,
+    css: `
+.a {
+  height: 100px;
+  padding: 20px;
+  overflow: scroll;
+}
+.b {
+  height: 50px;
+  width: 100px;
+}`,
+  }, {
+    label: 'Overflow top or left',
+    description: `If an element overflows to the top or left of a box then no scroll bars will appear to allow the user
+to scroll to it. It will just be invisible. Make the margin properties positive and you will see scroll bars appear to
+allow the content to be scrolled to.`,
+    html: `<div class="a blue">
+    <div class="b red"></div>
+</div>`,
+    css: `
+.a {
+  height: 100px;
+  padding: 20px;
+  overflow: auto;
+}
+.b {
+  height: 100px;
+  width: 100px;
+  margin-top: -75px;
+  margin-left: -75px;
+}`,
+  }, {
+    label: 'Nested overflow',
+    description: `When a child overflows multiple ancestors the first ancestor it overflows will be the one to scroll.`,
+    html: `<div class="a blue">
+    <div class="b red">
+        <div class="c green">
+    </div>
+</div>`,
+    css: `
+.a {
+  height: 100px;
+  padding: 20px;
+  overflow: auto;
+}
+.b {
+  height: 50px;
+  padding: 20px;
+  overflow: auto;
+}
+.c {
+  height: 200px;
+  width: 100px;
+}`,
+  }, {
+    label: 'Overflow of position: relative',
+    description: `You can still scroll to see elements that have been moved with <code>position: relative</code>.
+<br/><br/>
+Note how the padding is lost on the scrollable containing block though.`,
+    html: `<div class="a blue">
+    <div class="b red">
+    </div>
+</div>`,
+    css: `
+.a {
+  height: 100px;
+  padding: 20px;
+  overflow: auto;
+}
+.b {
+  height: 50px;
+  padding: 20px;
+  position: relative;
+  top: 100px;
+  left: 50px;
+}`,
+  }, {
+    label: 'Overflow of position: absolute',
+    description: `You can still scroll to see elements that have been moved with <code>position: absolute</code>. Note
+this is only the case if the ancestor is the containing block - remove the <code>position: relative</code> property on
+<code>.a</code> and see how it no longer scrolls. This is because the containing block is no longer <code>.a</code>`,
+    html: `<div class="a blue">
+    <div class="b red">
+    </div>
+</div>`,
+    css: `
+.a {
+  height: 100px;
+  padding: 20px;
+  overflow: auto;
+  position: relative;
+}
+.b {
+  height: 50px;
+  padding: 20px;
+  position: absolute;
+  top: 100px;
+  left: 50px;
 }`,
   }]
 })
