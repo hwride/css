8459bb6c8a94b47b4019feefbc6e44b9c9e2ce1f
Hugo Wride <hugowride@gmail.com>
2021-03-14 15:42:55 +0000

    Add more collapsing margin examples.

--- collapsing-margins/collapsing-margins.js
+++ collapsing-margins/collapsing-margins.js
@@ -1,13 +1,14 @@
 createCSSTestingComponent({
 	parent: document.querySelector('.example-collapsing-margins'),
-	description: `The top and bottom margins of the following two sibling elements collapse and use the same space.`,
+	description: `The top and bottom margins of the following two sibling elements collapse and use the same space. Note
+the larger of the two margins is the size used.`,
 	html: `<div class="a"></div>
 <div class="b"></div>`,
 	css: `
 .a {
   background: dodgerblue;
   height: 50px;
-  margin-bottom: 50px;
+  margin-bottom: 25px;
 }
 .b {
   background: red;
@@ -17,6 +18,25 @@ createCSSTestingComponent({
 	buttons: [{
 		label: 'Siblings',
 		reset: true
+	}, {
+		label: 'Siblings - no collapse',
+		description: `An element between two elements will prevent their margins from collapsing. See here how a 
+<code>&lt;br/&gt;</code> tag with no margins prevents collapsing.`,
+		html: `<div class="a"></div>
+<br/>
+<div class="b"></div>
+`,
+		css: `
+.a {
+  background: dodgerblue;
+  height: 50px;
+  margin-bottom: 25px;
+}
+.b {
+  background: red;
+  height: 50px;
+  margin-top: 50px;
+}`
 	}, {
 		label: 'Parent and child',
 		description: `Notice here how the child is not pushed away from the parent with its top margin. Instead the margins
@@ -33,6 +53,59 @@ collapse and parent itself is pushed down.`,
   background: red;
   height: 20px;
   margin-top: 100px;
+}`
+	}, {
+		label: 'Parent and child - no collapse',
+		description: `Padding or border on the parent will prevent margin collapsing.`,
+		html: `<div class="a">
+  <div class="b"></div>
+</div>`,
+		css: `
+.a {
+  background: dodgerblue;
+  height: 50px;
+  padding-top: 1px;
+}
+.b {
+  background: red;
+  height: 20px;
+  margin-top: 20px;
+}`
+	}, {
+		label: 'Negative margins',
+		description: `These collapse in the same way as positive margins. The largest of the negative margins is chosen for
+the final margin size.`,
+		html: `<div class="a"></div>
+<div class="b"></div>
+`,
+		css: `
+.a {
+  background: dodgerblue;
+  height: 50px;
+  margin-bottom: -5px;
+}
+.b {
+  background: red;
+  height: 50px;
+  margin-top: -25px;
+}`
+	}, {
+		label: 'Positive and negative margins',
+		description: `In this situation the final value is calculated by adding the margins together. Here the margins
+cancel each other out. This can be useful when you have to control over an existing margin and want to get rid of it.`,
+		html: `<div class="a"></div>
+<div class="b"></div>
+`,
+		css: `
+.a {
+  background: dodgerblue;
+  height: 50px;
+  margin-bottom: 25px;
+}
+.b {
+  background: red;
+  height: 50px;
+  margin-top: -25px;
 }`
 	}]
 })
