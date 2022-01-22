b2a1a318c6e2898fde0aea3c4022677a61f8db7a
Hugo Wride <hugowride@gmail.com>
2022-01-22 18:33:23 +0000

    Add viewport size testing page.

---
+++ viewport-size/viewport-size.js
@@ -0,0 +1,48 @@
+main()
+
+function main() {
+  const propTable = document.querySelector('.prop-table')
+  propTable.innerHTML = `
+<thead>
+  <tr>
+    <th>Property</th>
+    <th>Value</th>
+  </tr>
+</thead>
+<tbody></tbody>
+`
+  // Render table content for the first time.
+  renderTableContent()
+
+  // Register button to re-calculate the table values.
+  document.querySelector('.re-calculate-values').addEventListener('click', renderTableContent);
+}
+
+function renderTableContent() {
+  const tbody = document.querySelector('tbody')
+  tbody.innerHTML = ''
+  addRowExpression('document.documentElement.clientWidth')
+  addRowExpression('document.documentElement.getBoundingClientRect().width')
+  addRowExpression('window.innerWidth')
+  addRowExpression('window.outerWidth')
+  addRowExpression('document.documentElement.clientHeight')
+  addRowExpression('document.documentElement.getBoundingClientRect().height')
+  addRowExpression('window.innerHeight')
+  addRowExpression('window.outerHeight')
+  addRowKeyValue('(height: 100vh).clientHeight', document.querySelector('.height100vh').clientHeight)
+  addRowKeyValue('(height: 100%).clientHeight', document.querySelector('.height100percent').clientHeight)
+}
+
+function addRowExpression(expression) {
+  addRowKeyValue(expression, eval(expression));
+}
+
+function addRowKeyValue(key, value) {
+  const tbody = document.querySelector('tbody')
+  const tr = document.createElement('tr')
+  tr.innerHTML = `<tr>
+    <td><code>${key}</code></td>
+    <td><code>${value}</code></td>
+  </tr>`
+  tbody.appendChild(tr)
+}
