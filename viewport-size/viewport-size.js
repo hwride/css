655ca346b9b729122bfbaaeb1364e54d6ffad362
Hugo Wride <hugowride@gmail.com>
2022-01-23 22:55:15 +0000

    Viewport sizing test page improvements.
    Added inline display of height for easy reading.
    
    Also added auto update of this on browser scroll or resize.

--- viewport-size/viewport-size.js
+++ viewport-size/viewport-size.js
@@ -16,6 +16,11 @@ function main() {
 
   // Register button to re-calculate the table values.
   document.querySelector('.re-calculate-values').addEventListener('click', renderTableContent);
+
+  // Update el sizes for the first time.
+  updateElSizes()
+  document.addEventListener('scroll', updateElSizes)
+  window.addEventListener('resize', updateElSizes)
 }
 
 function renderTableContent() {
@@ -47,4 +52,17 @@ function addRowKeyValue(key, value) {
     <td><code>${value}</code></td>
   </tr>`
   tbody.appendChild(tr)
+}
+
+function updateElSizes() {
+  updateElSize('.height100percent')
+  updateElSize('.height100vh')
+  updateElSize('.height100percent-fixed')
+  updateElSize('.height100vh-fixed')
+}
+
+function updateElSize(selector) {
+  const el = document.querySelector(selector)
+  const newSize = el.clientHeight
+  el.querySelector('.size').innerHTML = ` - ${newSize}px`
 }
