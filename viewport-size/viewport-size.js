6a3d95efac87aa84fc485b2f19aaae8ee6251622
Hugo Wride <hugowride@gmail.com>
2022-01-24 23:36:50 +0000

    Add logging of visual viewport events.

--- viewport-size/viewport-size.js
+++ viewport-size/viewport-size.js
@@ -2,8 +2,10 @@ import {initTable, renderTableContent} from './table-utils.js'
 import {updateElSizes} from './test-el-utils.js';
 
 const eventsFired = {
-  resize: 0,
   scroll: 0,
+  resize: 0,
+  vvScroll: 0,
+  vvResize: 0,
 }
 
 main()
@@ -18,11 +20,13 @@ function main() {
 
   // Update values for the first time and on change.
   updateValues()
-  document.addEventListener('scroll', updateValues)
-  window.addEventListener('resize', updateValues)
+  document.addEventListener('scroll', updateValues.bind(null, 'scroll'))
+  window.addEventListener('resize', updateValues.bind(null, 'resize'))
+  window.visualViewport.addEventListener('scroll', updateValues.bind(null, 'vvScroll'))
+  window.visualViewport.addEventListener('resize', updateValues.bind(null, 'vvResize'))
 }
 
-function updateValues(evt) {
+function updateValues(evtName) {
   updateElSizes()
   const checkbox = document.querySelector('.dynamically-update-values')
   if(checkbox.checked) {
@@ -30,10 +34,11 @@ function updateValues(evt) {
   }
 
   // If an event fired log it.
-  if(evt) {
-    eventsFired[evt.type] = eventsFired[evt.type] != null ? eventsFired[evt.type] + 1 : 1;
-    document.querySelector('.resize-events-count').innerHTML = eventsFired.resize;
+  if(evtName) {
+    eventsFired[evtName] += 1;
     document.querySelector('.scroll-events-count').innerHTML = eventsFired.scroll;
+    document.querySelector('.resize-events-count').innerHTML = eventsFired.resize;
+    document.querySelector('.vv-scroll-events-count').innerHTML = eventsFired.vvScroll;
+    document.querySelector('.vv-resize-events-count').innerHTML = eventsFired.vvResize;
   }
-  console.log(eventsFired);
 }
