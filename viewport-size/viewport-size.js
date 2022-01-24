8fdf40e43383ac7695c12d04b9202565fd227641
Hugo Wride <hugowride@gmail.com>
2022-01-24 23:28:39 +0000

    Viewport sizing - add log of event counts.
    Can see which actions cause certain events.

--- viewport-size/viewport-size.js
+++ viewport-size/viewport-size.js
@@ -1,6 +1,11 @@
 import {initTable, renderTableContent} from './table-utils.js'
 import {updateElSizes} from './test-el-utils.js';
 
+const eventsFired = {
+  resize: 0,
+  scroll: 0,
+}
+
 main()
 
 function main() {
@@ -9,7 +14,7 @@ function main() {
   renderTableContent()
 
   // Register button to re-calculate the table values.
-  document.querySelector('.re-calculate-values').addEventListener('click', renderTableContent);
+  document.querySelector('.re-calculate-values').addEventListener('click', renderTableContent)
 
   // Update values for the first time and on change.
   updateValues()
@@ -17,10 +22,18 @@ function main() {
   window.addEventListener('resize', updateValues)
 }
 
-function updateValues() {
+function updateValues(evt) {
   updateElSizes()
   const checkbox = document.querySelector('.dynamically-update-values')
   if(checkbox.checked) {
     renderTableContent()
   }
+
+  // If an event fired log it.
+  if(evt) {
+    eventsFired[evt.type] = eventsFired[evt.type] != null ? eventsFired[evt.type] + 1 : 1;
+    document.querySelector('.resize-events-count').innerHTML = eventsFired.resize;
+    document.querySelector('.scroll-events-count').innerHTML = eventsFired.scroll;
+  }
+  console.log(eventsFired);
 }
