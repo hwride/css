2d05a2f466f55f57123c137549a133d08c77b249
Hugo Wride <hugowride@gmail.com>
2022-08-22 21:13:07 +0100

    Add animation-fill-mode example.

--- animation/keyframes.js
+++ animation/keyframes.js
@@ -177,5 +177,142 @@ ${basicKeyframe}
 	animation-iteration-count: infinite;
 	animation-direction: alternate;
 }`
+	}, {
+		label: 'Fill mode - forwards',
+		description: `<p>
+	Normally the properties we specify in keyframes only apply while the animation is running. This means if we animated
+	a property to be another value, it would reset to whatever the normal value is after the animation is over.
+</p>
+<p>
+	You can specify <code>animation-fill-mode: forwards</code> to change this behaviour, and have the final values of
+	properties set in keyframe animations persist after the animation is over.
+</p>
+<p>
+	Notice here how only the second div remains with opacity 0 after the animation as finished. 
+</p>`,
+		html: `<div class="animate"></div>
+<div class="animate fill-mode-forwards"></div>`,
+		css: `
+@keyframes fade-out {
+	from {
+		opacity: 1;
+	}
+	to {
+		opacity: 0;
+	}
+}
+.animate {
+	background: dodgerblue;
+	width: 50px;
+	height: 50px;
+	animation: fade-out 1500ms;
+}
+.fill-mode-forwards {
+	animation-fill-mode: forwards;
+	margin-top: 8px;
+}`
+	}, {
+		label: 'Fill mode - backwards',
+		description: `<p>
+	This is opposite of <code>forwards</code>, and will start the element with any properties an animation would begin
+	with.
+</p>
+<p>
+	Notice here how the top element begins visible, and only turns invisible when the animation beings, causing a flashing
+	effect, rather than just fading in. When <code>animation-fill-mode: backwards</code> is enabled it begins faded out.
+</p>`,
+		html: `<div class="animate"></div>
+<div class="animate fill-mode-backwards"></div>`,
+		css: `
+@keyframes fade-out {
+	from {
+		opacity: 0;
+	}
+	to {
+		opacity: 1;
+	}
+}
+.animate {
+	background: dodgerblue;
+	width: 50px;
+	height: 50px;
+	animation: fade-out 1500ms;
+	animation-delay: 500ms;
+}
+.fill-mode-backwards {
+	animation-fill-mode: backwards;
+	margin-top: 8px;
+}`
+	}, {
+		label: 'Fill mode - both',
+		description: `<p>
+	This causes the animation to begin and end with properties that the animation would set. This is a good value to use
+	as a default for your keyframe animations as you generally want this behaviour.
+</p>
+<p>
+	The intention here is the blue squares start at the start line, animate to the middle line for the middle of the 
+	animation then end up back at the end line. Notice here how: 
+</p>
+<ol>
+	<li>The top item both begins and ends at the middle line which is incorrect.</li>
+	<li>The second item start correctly, but ends at the middle line which is incorrect.</li>
+	<li>The third item ends correctly, but starts at the middle line which is incorrect.</li>
+	<li>The fourth item appears correctly for the whole animation.</li>
+</ol>`,
+		html: `<div class="wrapper">
+	<div class="animate"></div>
+	<div class="animate fill-mode-backwards"></div>
+	<div class="animate fill-mode-forwards"></div>
+	<div class="animate fill-mode-both"></div>
+</div>`,
+		css: `@keyframes fade-out {
+	0% {
+		transform: translateX(50px);
+	}
+	50% {
+		transform: translateX(0);
+	}
+	100% {
+		transform: translateX(50px);
+	}
+}
+.animate {
+	background: dodgerblue;
+	width: 50px;
+	height: 50px;
+	animation: fade-out 1500ms;
+	animation-delay: 500ms;
+	margin-bottom: 8px;
+}
+.fill-mode-backwards {
+	animation-fill-mode: backwards;
+}
+.fill-mode-forwards {
+	animation-fill-mode: forwards;
+}
+.fill-mode-both {
+	animation-fill-mode: both;
+}
+
+
+.wrapper {
+    display: inline-block;
+    position: relative;
+    width: 100px;
+    margin-left: 50px;
+    border-left: 1px solid black;
+    border-right: 1px solid black;
+}
+.wrapper:before {
+    content: 'Middle';
+    position: absolute;
+    left: -3rem;
+}
+.wrapper:after {
+    content: 'Start and end';
+    position: absolute;
+    right: -11ch;
+    top: 0;
+}`
 	}]
 })
