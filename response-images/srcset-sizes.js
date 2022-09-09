ff99ce2567b44f544352df53e9d15902c0c3eda5
Hugo Wride <hugowride@gmail.com>
2022-09-09 17:40:32 +0100

    Add more to results table.

--- response-images/srcset-sizes.js
+++ response-images/srcset-sizes.js
@@ -1,8 +1,8 @@
 function updateDeviceInfo() {
   document.querySelector('.device-pixel-ratio').innerHTML = window.devicePixelRatio;
   document.querySelector('.window-outer-width').innerHTML = window.outerWidth;
-  document.querySelector('.device-pixels-outer').innerHTML = window.devicePixelRatio * window.outerWidth;
   document.querySelector('.window-inner-width').innerHTML = window.innerWidth;
+  document.querySelector('.device-pixels-outer').innerHTML = window.devicePixelRatio * window.outerWidth;
   document.querySelector('.device-pixels-inner').innerHTML = window.devicePixelRatio * window.innerWidth;
 }
 updateDeviceInfo()
