3303de13d92cc9b379e9b5eac598882a312afe0e
Hugo Wride <hugowride@gmail.com>
2022-09-09 07:44:48 +0100

    Add more info to the info panel.

--- response-images/srcset-sizes.js
+++ response-images/srcset-sizes.js
@@ -1,2 +1,8 @@
-document.querySelector('.device-pixel-ratio').innerHTML = window.devicePixelRatio;
-document.querySelector('.window-outer-width').innerHTML = window.outerWidth;
+function updateDeviceInfo() {
+  document.querySelector('.device-pixel-ratio').innerHTML = window.devicePixelRatio;
+  document.querySelector('.window-outer-width').innerHTML = window.outerWidth;
+  document.querySelector('.device-pixels').innerHTML = window.devicePixelRatio * window.outerWidth;
+}
+updateDeviceInfo()
+
+window.addEventListener('resize', updateDeviceInfo)
