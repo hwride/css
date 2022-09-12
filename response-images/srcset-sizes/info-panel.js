ecc098756a4b0bc2a40413cb68de22a2ac7c7bbd
Hugo Wride <hugowride@gmail.com>
2022-09-12 17:36:58 +0100

    Move examples of responsive images to separate pages.
    The caching of images was interfering with testing the final image
    chosen for each example.

---
+++ response-images/srcset-sizes/info-panel.js
@@ -0,0 +1,39 @@
+const infoPanelHtml = `
+<div class="info">
+	<table>
+		<tr>
+			<td><code>window.innerWidth</code></td>
+			<td><span class="window-inner-width"></span></td>
+		</tr>
+		<tr>
+			<td><code>devicePixelRatio * innerWidth</code></td>
+			<td><span class="device-pixels-inner"></span></td>
+		</tr>
+		<tr>
+			<td><code>window.devicePixelRatio</code></td>
+			<td><span class="device-pixel-ratio"></span></td>
+		</tr>
+		<tr>
+			<td><code>window.outerWidth</code></td>
+			<td><span class="window-outer-width"></span></td>
+		</tr>
+		<tr>
+			<td><code>devicePixelRatio * outerWidth</code></td>
+			<td><span class="device-pixels-outer"></span></td>
+		</tr>
+	</table>
+</div>
+`;
+document.body.innerHTML += infoPanelHtml;
+
+/* Hook up device info panel */
+function updateDeviceInfo() {
+  document.querySelector('.device-pixel-ratio').innerHTML = window.devicePixelRatio;
+  document.querySelector('.window-outer-width').innerHTML = window.outerWidth;
+  document.querySelector('.window-inner-width').innerHTML = window.innerWidth;
+  document.querySelector('.device-pixels-outer').innerHTML = window.devicePixelRatio * window.outerWidth;
+  document.querySelector('.device-pixels-inner').innerHTML = window.devicePixelRatio * window.innerWidth;
+}
+updateDeviceInfo()
+
+window.addEventListener('resize', updateDeviceInfo)
--- response-images/srcset-sizes/srcset-sizes.js
+++ response-images/srcset-sizes/srcset-sizes.js
@@ -1,15 +1,3 @@
-/* Hook up device info panel */
-function updateDeviceInfo() {
-  document.querySelector('.device-pixel-ratio').innerHTML = window.devicePixelRatio;
-  document.querySelector('.window-outer-width').innerHTML = window.outerWidth;
-  document.querySelector('.window-inner-width').innerHTML = window.innerWidth;
-  document.querySelector('.device-pixels-outer').innerHTML = window.devicePixelRatio * window.outerWidth;
-  document.querySelector('.device-pixels-inner').innerHTML = window.devicePixelRatio * window.innerWidth;
-}
-updateDeviceInfo()
-
-window.addEventListener('resize', updateDeviceInfo)
-
 /* Populate results table */
 const headers = [
   { name: 'device', label: 'Device' },
