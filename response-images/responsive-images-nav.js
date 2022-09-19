7f0779af97ef3e22f5f3a151e3cb75df7cb2780c
Hugo Wride <hugowride@gmail.com>
2022-09-19 14:47:46 +0100

    Re-organise responsive image examples layout.

---
+++ response-images/responsive-images-nav.js
@@ -0,0 +1,4 @@
+createNav(undefined, [{
+  href: '/css/response-images/index.html',
+  label: 'Response images'
+}])
--- response-images/srcset-sizes/srcset-sizes-nav.js
+++
@@ -1,4 +0,0 @@
-createNav(undefined, [{
-  href: '/css/response-images/srcset-sizes/srcset-sizes.html',
-  label: 'Response images with srcset and sizes'
-}])
