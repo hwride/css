e9826dc95e86c661e9c753d12543eec61e24fa4a
Hugo Wride <hugowride@gmail.com>
2025-03-27 08:28:07 +0000

    Add inline-size container query example

--- container-queries/container-queries.js
+++ container-queries/container-queries.js
@@ -33,5 +33,40 @@ createCSSTestingComponent({
 	}
 }
 `
+	}, {
+		label: 'Change background with container-type: inline-size',
+		html: `<div class="container">
+  <div class="child"></div>
+</div>`,
+		description: `
+<p>
+	When using <code>container-type: inline-size</code> the <b>height</b> of a container will still adjust based on its 
+	contents (but not the width). You can test this by adding <code>width: fit-content;</code> to 
+	<code>.container</code> and see that it shrinks below the width of its content, but the height still wraps content.
+</p>
+<p>
+	The idea behind these different properties is to allow us to say what the container will ignore from its children
+	to avoid potential infinite loops with container queries. This can happen for example if you have a container
+	query based on width, which causes a child to increase in size and negate the container query rule, causing an 
+	infinite loop.
+</p>
+`,
+		css: `
+.container {
+	container-type: inline-size;
+	border: 1px dotted black;
+}
+.child {
+	width: 50px;
+	height: 50px;
+	background: dodgerblue;
+}
+
+@container (max-width: 100px) {
+	.child {
+		background: salmon;
+	}
+}
+`
 	}]
 })
