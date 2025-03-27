13e3fbc094d6f6436605b65a05cccd39c50ea48d
Hugo Wride <hugowride@gmail.com>
2025-03-27 08:20:02 +0000

    Clarify container-size first example

--- container-queries/container-queries.js
+++ container-queries/container-queries.js
@@ -1,12 +1,18 @@
 createCSSTestingComponent({
 	parent: document.querySelector('.container-queries'),
 	buttons: [{
-		label: 'Change background',
+		label: 'Change background with container-type: size',
 		html: `<div class="container">
   <div class="child"></div>
 </div>`,
-		description:
-			`Try changing the width of the container to be <code>100px</code> or less to see the container query apply.`,
+		description: `
+<p>Try changing the width of the container to be <code>100px</code> or less to see the container query apply.</p>
+<p>
+	Also note how if you use <code>container-type: size</code> a container will no longer use its children in its size
+	calculations. Remove the width and height from the <code>.container</code> CSS and note how the height does not wrap
+	its child as it would do normally.
+</p>
+`,
 		reset: true,
 		css: `
 .container {
